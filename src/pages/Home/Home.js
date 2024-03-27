f5868df5ab62978d6b27e862ee146309ca95fa00
HongHao <honghaocp@gmail.com>
2024-03-27 21:46:23 +0700

    add behavior to Home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,12 @@
+import { useEffect } from "react";
+
 function Home() {
+  const printEample = () => {
+    console.log("Example");
+  };
+  useEffect(() => {
+    printEample();
+  }, []);
   return <div>Home tttt</div>;
 }
 
